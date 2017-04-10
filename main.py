24ffa67fe535253034dc8e5677cacf5bdeaed340
Your Name <you@example.com>
2017-04-10 17:50:13 +0300

    Version 1.1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import face_recognition
 
 from PIL import Image
@@ -8,32 +9,43 @@ from time import time
 start_time = time()
 print ('Start recognition')
 
-all_persons = []
+# add argument parser for images
+ap = argparse.ArgumentParser()
+ap.add_argument('-p', '--person', help='path to name_surname.jpg file')
+ap.add_argument('-f', '--folder', help='path to files folder')
+args = vars(ap.parse_args())
+
+name_folder = args['person'].split('.')
 
 persons_folder = './persons'
-photos_folder = './photos'
-results_folder = './results'
+photos_folder = args['folder']
 
-# encode person faces
-for filename in os.listdir(persons_folder):
-    person = face_recognition.load_image_file(os.path.join(persons_folder, filename))
-    person_encoding = face_recognition.face_encodings(person)[0]
+try:
+    results_folder = os.stat('{}_folder'.format(name_folder[0]))
+except:
+    results_folder = os.mkdir('{}_folder'.format(name_folder[0]))
 
-    all_persons.append(person_encoding)
+# encode person face
+person = face_recognition.load_image_file(os.path.join(persons_folder, args['person']))
+person_encoding = face_recognition.face_encodings(person)[0]
 
 # find and save similar faces
 for filename in os.listdir(photos_folder):
-    for people in all_persons:
-        unknown_picture = face_recognition.load_image_file(os.path.join(photos_folder, filename))
+    unknown_picture = face_recognition.load_image_file(os.path.join(photos_folder, filename))
+
+    try:
         unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+    except(IndexError):
+        continue
 
-        results = face_recognition.compare_faces([people], unknown_face_encoding)
+    results = face_recognition.compare_faces([person_encoding], unknown_face_encoding)
 
-        if results[0]:
-            unknown_picture = Image.fromarray(unknown_picture)
-            unknown_picture.save(os.path.join(results_folder, filename))
-        else:
-            pass
+    if results[0]:
+        unknown_picture = Image.fromarray(unknown_picture)
+        unknown_picture.save(os.path.join('{}_folder'.format(name_folder[0]), filename))
+        print ('Find {} on {}'.format(name_folder[0], filename))
+    else:
+        pass
 
 #end timer
 end_time = time() - start_time
